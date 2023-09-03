1559e36e1787b79dc729f10036047698294e7cb0
maturwe <petermaturwe@gmail.com>
2023-09-03 15:12:00 +0300

    added fonts and also job types

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,29 @@
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
+import { useCallback } from 'react';
+import {useFonts} from 'expo-font';
+import * as SplashScreeen from 'expo-splash-screen';
+
+
+SplashScreeen.preventAutoHideAsync();
+
 
 
 const Layout = () => {
-    return <Stack />;
+    const [fontsLoaded] = useFonts({
+        DMBold : require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium : require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular : require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+    const onLayoutRootView = useCallback(async () =>{
+        if(fontsLoaded){
+            await SplashScreen.hideAsync();
+        }
+    },
+    [fontsLoaded]
+    )
+    if (!fontsLoaded) return null;
+    return <Stack onLayout = {onLayoutRootView} />;
+    
 
 }
 
